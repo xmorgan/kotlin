d2bd2b9a3529b4a31571f8f7b218862639381401
Andrei Klunnyi <andrei.klunnyi@jetbrains.com>
2020-05-05 14:24:16 +0200

    KT-32368 Rework Inline hints settings

--- idea/src/org/jetbrains/kotlin/idea/parameterInfo/LambdaReturnValueHints.kt
+++ idea/src/org/jetbrains/kotlin/idea/parameterInfo/LambdaReturnValueHints.kt
@@ -10,6 +10,7 @@ import com.intellij.psi.PsiWhiteSpace
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
 import org.jetbrains.kotlin.idea.core.util.isOneLiner
 import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.endOffset
 import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
 import org.jetbrains.kotlin.psi.psiUtil.getStrictParentOfType
 import org.jetbrains.kotlin.psi.psiUtil.startOffset
@@ -46,7 +47,9 @@ fun provideLambdaReturnValueHints(expression: KtExpression): List<InlayInfo> {
     val bindingContext = expression.analyze(BodyResolveMode.PARTIAL_WITH_CFA)
     if (expression.isUsedAsResultOfLambda(bindingContext)) {
         val lambdaName = getNameOfFunctionThatTakesLambda(expression) ?: "lambda"
-        return listOf(InlayInfo("$TYPE_INFO_PREFIX^$lambdaName", expression.startOffset))
+        return listOf(
+            InlayInfo("$TYPE_INFO_PREFIX^$lambdaName", expression.endOffset)
+        )
     }
     return emptyList()
 }
