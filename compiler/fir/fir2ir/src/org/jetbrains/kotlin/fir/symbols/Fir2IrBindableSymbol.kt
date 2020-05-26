f9bb8f5fd1ab2a68615f3dd9db5c7b4301c6104a
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-05-26 13:08:37 +0300

    [FIR2IR] Create descriptor in bindable symbol lazily

--- compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/symbols/Fir2IrBindableSymbol.kt
+++ compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/symbols/Fir2IrBindableSymbol.kt
@@ -22,37 +22,9 @@ abstract class Fir2IrBindableSymbol<out D : DeclarationDescriptor, B : IrSymbolO
     override val owner: B
         get() = _owner ?: throw IllegalStateException("Symbol is unbound")
 
-    private var _descriptor: D? = null
-
     override fun bind(owner: B) {
         if (_owner == null) {
             _owner = owner
-            _descriptor = when (owner) {
-                is IrEnumEntry -> WrappedEnumEntryDescriptor()
-                is IrClass -> WrappedClassDescriptor()
-                is IrConstructor -> WrappedClassConstructorDescriptor()
-                is IrFunction -> when {
-                    owner.name.isSpecial && owner.name.asString().startsWith("get") ->
-                        WrappedPropertyGetterDescriptor(Annotations.EMPTY, SourceElement.NO_SOURCE)
-                    owner.name.isSpecial && owner.name.asString().startsWith("set") ->
-                        WrappedPropertySetterDescriptor(Annotations.EMPTY, SourceElement.NO_SOURCE)
-                    else -> if (containerSource != null) {
-                        WrappedFunctionDescriptorWithContainerSource(containerSource)
-                    } else {
-                        WrappedSimpleFunctionDescriptor()
-                    }
-                }
-                is IrVariable -> WrappedVariableDescriptor()
-                is IrValueParameter -> WrappedValueParameterDescriptor()
-                is IrTypeParameter -> WrappedTypeParameterDescriptor()
-                is IrProperty -> if (containerSource != null) {
-                    WrappedPropertyDescriptorWithContainerSource(containerSource)
-                } else {
-                    WrappedPropertyDescriptor()
-                }
-                is IrField -> WrappedFieldDescriptor()
-                else -> throw IllegalStateException("Unsupported owner in Fir2IrBindableSymbol: $owner")
-            } as D
         } else {
             throw IllegalStateException("${javaClass.simpleName} is already bound")
         }
@@ -66,6 +38,32 @@ abstract class Fir2IrBindableSymbol<out D : DeclarationDescriptor, B : IrSymbolO
     override val isBound: Boolean
         get() = _owner != null
 
-    override val descriptor: D
-        get() = _descriptor ?: throw IllegalStateException("Symbol is unbound")
+    override val descriptor: D by lazy {
+        when (val owner = owner) {
+            is IrEnumEntry -> WrappedEnumEntryDescriptor().apply { bind(owner) }
+            is IrClass -> WrappedClassDescriptor().apply { bind(owner) }
+            is IrConstructor -> WrappedClassConstructorDescriptor().apply { bind(owner) }
+            is IrSimpleFunction -> when {
+                owner.name.isSpecial && owner.name.asString().startsWith("get") ->
+                    WrappedPropertyGetterDescriptor(Annotations.EMPTY, SourceElement.NO_SOURCE)
+                owner.name.isSpecial && owner.name.asString().startsWith("set") ->
+                    WrappedPropertySetterDescriptor(Annotations.EMPTY, SourceElement.NO_SOURCE)
+                else -> if (containerSource != null) {
+                    WrappedFunctionDescriptorWithContainerSource(containerSource)
+                } else {
+                    WrappedSimpleFunctionDescriptor()
+                }
+            }.apply { bind(owner) }
+            is IrVariable -> WrappedVariableDescriptor().apply { bind(owner) }
+            is IrValueParameter -> WrappedValueParameterDescriptor().apply { bind(owner) }
+            is IrTypeParameter -> WrappedTypeParameterDescriptor().apply { bind(owner) }
+            is IrProperty -> if (containerSource != null) {
+                WrappedPropertyDescriptorWithContainerSource(containerSource)
+            } else {
+                WrappedPropertyDescriptor()
+            }.apply { bind(owner) }
+            is IrField -> WrappedFieldDescriptor().apply { bind(owner) }
+            else -> throw IllegalStateException("Unsupported owner in Fir2IrBindableSymbol: $owner")
+        } as D
+    }
 }
