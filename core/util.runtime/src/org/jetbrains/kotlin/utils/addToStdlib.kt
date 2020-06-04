fde63ee3314733b9ae1e8fbab479fc91f264e0c0
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-06-04 12:38:06 +0300

    [FIR] Add `ConeAttributes` to `ConeKotlinType`

--- core/util.runtime/src/org/jetbrains/kotlin/utils/addToStdlib.kt
+++ core/util.runtime/src/org/jetbrains/kotlin/utils/addToStdlib.kt
@@ -162,3 +162,12 @@ inline fun <T, R> Iterable<T>.same(extractor: (T) -> R): Boolean {
     }
     return true
 }
+
+inline fun <T, R> Collection<T>.foldMap(transform: (T) -> R, operation: (R, R) -> R): R {
+    val iterator = iterator()
+    var result = transform(iterator.next())
+    while (iterator.hasNext()) {
+        result = operation(result, transform(iterator.next()))
+    }
+    return result
+}
