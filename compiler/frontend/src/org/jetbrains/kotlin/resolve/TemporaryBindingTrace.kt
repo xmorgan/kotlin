7512a6670a7dda74bcfe5f6a750d3e531851e53d
Victor Petukhov <victor.petukhov@jetbrains.com>
2020-06-10 13:21:42 +0300

    Rewrite `TemporaryBindingTrace` to Kotlin

---
+++ compiler/frontend/src/org/jetbrains/kotlin/resolve/TemporaryBindingTrace.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.resolve
+
+import org.jetbrains.kotlin.resolve.BindingTraceFilter.Companion.ACCEPT_ALL
+
+class TemporaryBindingTrace(val trace: BindingTrace, debugName: String, filter: BindingTraceFilter) :
+    DelegatingBindingTrace(trace.bindingContext, debugName, true, filter, false) {
+    fun commit() {
+        addOwnDataTo(trace)
+        clear()
+    }
+
+    fun commit(filter: TraceEntryFilter, commitDiagnostics: Boolean) {
+        addOwnDataTo(trace, filter, commitDiagnostics)
+        clear()
+    }
+
+    override fun wantsDiagnostics(): Boolean {
+        return trace.wantsDiagnostics()
+    }
+
+    companion object {
+        @JvmOverloads
+        @JvmStatic
+        fun create(trace: BindingTrace, debugName: String, filter: BindingTraceFilter = ACCEPT_ALL): TemporaryBindingTrace {
+            return TemporaryBindingTrace(trace, debugName, filter)
+        }
+
+        @JvmStatic
+        fun create(trace: BindingTrace, debugName: String, resolutionSubjectForMessage: Any?): TemporaryBindingTrace {
+            return create(trace, AnalyzingUtils.formDebugNameForBindingTrace(debugName, resolutionSubjectForMessage))
+        }
+    }
+}
